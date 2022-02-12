75060bc35eff2ada45dca9d137f1810202aaa573
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-02-12 10:38:01 +0530

    started comment

---
+++ comment-react-revision/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Comments from "./Componants/Comments";
+
+function App() {
+  return (
+    <div className="App">
+      <Comments />
+    </div>
+  );
+}
+
+export default App;
---
+++ comment-react-revision/src/Componants/Comments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Comments() {
+  return (
+    <div>
+        <input type="text" placeholder="add comment"/>
+    </div>
+  )
+}
+
+export default Comments
