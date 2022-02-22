a1b592247745450a902616ed5832e49228457435
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-02-22 18:53:12 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,1 +1,14 @@
 //mongo intro
+var pipeline = [
+    {$sort:{"age":1}}
+]
+db.leads.aggregate(pipeline)
+var pipeline = [
+    {$sort:{"age":-1}},
+    {$limit:3}
+]
+db.leads.aggregate(pipeline)
+
+//.find().sort({})
+
+//
